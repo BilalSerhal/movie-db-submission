843de14263918349359402208ff8853870629886
BilalSerhal <bilal111.rg@gmail.com>
2022-12-28 19:54:42 +0200

    create an express simple API

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const port=3000
 app.get('/', (req, res) => {
     res.send('ok')
   })
+  app.get('/test', (req, res) => {
+    res.send('{status:200, message:"ok}');
+  })
+
+  app.get('/time', (req, res) => { 
+     res.send('{status:200,message : <time> 14:20  </time>}');
+})
 
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`)
