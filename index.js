e5a8ffc094fd477c96c51af1c934c7c526b9cd35
BilalSerhal <bilal111.rg@gmail.com>
2022-12-28 20:15:38 +0200

    let's complicate the api

--- index.js
+++ index.js
@@ -13,6 +13,28 @@ app.get('/', (req, res) => {
      res.send('{status:200,message : <time> 14:20  </time>}');
 })
 
+app.get("/hello/:id", (req,res)=>{
+    const {id} = req.params;
+    res.send(`{status:200, message: ${id} }`) 
+ })
+
+ app.get('/search',(req,res) => {
+    const search = req.query.s;
+    if (typeof search != 'undefined') {
+        const response = {
+            status:200, message:"ok", data: search
+        };
+        res.send(response);
+    }
+    else {
+        const response = {
+            status:500, error:true, message: "you have to provide a search"
+        };
+        res.status(500);
+        res.send(response);
+    }
+});
+
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`)
 })
